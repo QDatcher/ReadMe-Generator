6e6217952b838bb23817fc9803fb879a20e974f3
Quise <darkquise@gmail.com>
2023-04-04 17:09:13 -0400

    added more inputs for user to fill out

--- index.js
+++ index.js
@@ -10,7 +10,30 @@ inquirer
           },
           {
             type: 'input',
-            name: 'name',
-            message: 'What is your name?',
+            name: 'description',
+            message: 'Give us a description of your project:',
+          },
+          {
+            type: 'input',
+            name: 'githubUser',
+            message: 'What is your Github username?',
+          },
+          {
+            type: 'input',
+            name: 'installation',
+            message: 'What do you need to install for project to function (If nothing leave blank)',
+          },
+          {
+            type: 'input',
+            name: 'usage',
+            message: 'Give us a depiction of how to use your project:',
+          },
+          {
+            type: 'input',
+            name: 'description',
+            message: 'Give us a description of your project:',
           },
     ])
+    .then((data) =>{
+
+    })
