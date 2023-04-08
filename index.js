4a27a73346c0565a7318d5eb4a82bf5cc94f3cbc
Quise <darkquise@gmail.com>
2023-04-08 18:19:47 -0400

    added read me generator and 2 extra inputs

--- index.js
+++ index.js
@@ -1,39 +1,121 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const generateReadMe = (title, description, usage, installation,testInstructions) => {
+    return `
+    # ${title}
+
+    ## Description
+    
+        ${description}
+    
+    ## Table of Contents (Optional)
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    - [Questions](#questions)
+    
+    ## Installation
+    
+        ${installation}
+
+    ## Usage
+    
+        ${usage}
+    
+    ## Credits
+    
+    List your collaborators, if any, with links to their GitHub profiles.
+    
+    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+    
+    If you followed tutorials, include links to those here as well.
+    
+    ## License
+    
+    The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+    
+    ---
+    
+    🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+    
+    ## Badges
+    
+    ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+    
+    Badges aren't necessary, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+    
+    ## Features
+    
+    If your project has a lot of features, list them here.
+    
+    ## How to Contribute
+    
+    If you created an application or package and would like other developers to contribute to it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+    
+    ## Tests
+    
+    ${testInstructions}
+    
+    ## Questions
+    
+    If you have any additional questions or want to see some previous works of mine you can visit my github or contact me through email
+    
+        -GitHub Account: https://github.com/${githubUser}
+        -Email: `
+}
+
+var questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the name of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Give us a description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'githubUser',
+        message: 'What is your Github username?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What do you need to install for project to function (If nothing leave blank)',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Give us a depiction of how to use your project:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+      {
+        type: 'input',
+        name: 'testInstructions',
+        message: 'Give instructions to use the test',
+      },
+      {
+        type: 'list',
+        name: 'badge',
+        choices: [],
+      },
+      
+]
+
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'projectTitle',
-            message: 'What is the name of your project?',
-          },
-          {
-            type: 'input',
-            name: 'description',
-            message: 'Give us a description of your project:',
-          },
-          {
-            type: 'input',
-            name: 'githubUser',
-            message: 'What is your Github username?',
-          },
-          {
-            type: 'input',
-            name: 'installation',
-            message: 'What do you need to install for project to function (If nothing leave blank)',
-          },
-          {
-            type: 'input',
-            name: 'usage',
-            message: 'Give us a depiction of how to use your project:',
-          },
-          {
-            type: 'input',
-            name: 'description',
-            message: 'Give us a description of your project:',
-          },
-    ])
-    .then((data) =>{
+    .prompt(questions)
+    .then((answers) =>{
+        var license;
+
+        
 
     })
